a1764584b5b94e2e1420f7c54b18a2db43f983f4
loinguyeniopan <116430777+loinguyeniopan@users.noreply.github.com>
2025-02-16 21:57:41 +0700

    Update dada2_workflow.R

--- lib/dada2_workflow.R
+++ lib/dada2_workflow.R
@@ -1,21 +1,26 @@
 # Load the DADA2 processing pipeline function from an external script
 source("dada2_run.R")  # Ensures modularity and reusability
 
-# Set working directory path
+# Set working directory path and input
 path <- getwd()  # Gets the current working directory
+t2s_file <- file.path(path, "t2sENI.csv") # Metadata file mapping tags to samples
 
-# Define file paths for input and output data
-t2s_file <- file.path(path, "t2sENI.csv")         # Metadata file mapping tags to samples
-asv_table <- file.path(path, "ENI_ASV_table.csv") # Output file for ASV table
-stats_file <- file.path(path, "ENI_filter_stats.csv") # Output file for filtering statistics
+
+# Define file paths for output data
+# Create output directory
+today_date <- format(Sys.time(), "%Y%m%d_%H%M")
+output_dir <- paste0("DADA2_", today_date)
+if (!dir.exists(output_dir)) {
+  dir.create(output_dir)
+}
+
+asv_table <- file.path(output_dir, "ENI_ASV_table.csv") # Output file for ASV table
+stats_file <- file.path(output_dir, "ENI_filter_stats.csv") # Output file for filtering statistics
+repseq_file <- file.path(output_dir, "ENI_representative_sequences.fasta") # Output file for representative sequences
 
 # Set processing options
 cpus <- TRUE  # Enable multi-threading (set actual number if needed)
 pool <- "pseudo"  # DADA2 pooling method for denoising
-lib_list <- unique(t2s$run)  # Identify unique sequencing libraries in metadata
-
-# Display message about the number of libraries being processed
-message(paste("DADA2 analysis for", length(lib_list), "libraries."))
 
 # Run the DADA2 pipeline with defined inputs and parameters
 run_dada2_pipeline(path, t2s_file, asv_table, stats_file, cpus = cpus)
