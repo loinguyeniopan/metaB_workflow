d5ba8dbfa8d4dc00598b34517dd4629eb14a542b
loinguyeniopan <116430777+loinguyeniopan@users.noreply.github.com>
2025-02-16 23:08:32 +0700

    Update dada2_workflow.R

--- lib/dada2_workflow.R
+++ lib/dada2_workflow.R
@@ -3,7 +3,7 @@ source("dada2_run.R")  # Ensures modularity and reusability
 
 # Set working directory path and input
 path <- getwd()  # Gets the current working directory
-t2s_file <- file.path(path, "t2sENI.csv") # Metadata file mapping tags to samples
+t2s_file <- file.path(path, "t2s.csv") # Metadata file mapping tags to samples
 
 
 # Define file paths for output data
@@ -14,9 +14,9 @@ if (!dir.exists(output_dir)) {
   dir.create(output_dir)
 }
 
-asv_table <- file.path(output_dir, "ENI_ASV_table.csv") # Output file for ASV table
-stats_file <- file.path(output_dir, "ENI_filter_stats.csv") # Output file for filtering statistics
-repseq_file <- file.path(output_dir, "ENI_representative_sequences.fasta") # Output file for representative sequences
+asv_table <- file.path(output_dir, "ASV_table.csv") # Output file for ASV table
+stats_file <- file.path(output_dir, "filter_stats.csv") # Output file for filtering statistics
+repseq_file <- file.path(output_dir, "representative_sequences.fasta") # Output file for representative sequences
 
 # Set processing options
 cpus <- TRUE  # Enable multi-threading (set actual number if needed)
